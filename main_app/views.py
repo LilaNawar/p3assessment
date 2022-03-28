105e040d96dbf3a30a6c8f9a8b5e0e88710f8f85
saadkhan29 <saad.iqbal@generalassemb.ly>
2022-03-28 12:24:54 -0400

    Cat Collector Models checkpoint 02

--- main_app/views.py
+++ main_app/views.py
@@ -1,20 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Cat
 # Create your views here.
 
-class Cat:
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
+# class Cat:
+#     def __init__(self, name, breed, description, age):
+#         self.name = name
+#         self.breed = breed
+#         self.description = description
+#         self.age = age
 
-cats = [
-  Cat('Lolo', 'tabby', 'foul little demon', 3),
-  Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
-  Cat('Raven', 'black tripod', '3 legged cat', 4)
-]
+# cats = [
+#   Cat('Lolo', 'tabby', 'foul little demon', 3),
+#   Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
+#   Cat('Raven', 'black tripod', '3 legged cat', 4)
+# ]
 
 def home(request):
     return render(request, 'home.html')
@@ -23,4 +24,5 @@ def about(request):
   return render(request, 'about.html')
 
 def cats_index(request):
+    cats = Cat.objects.all()
     return render(request, 'cats/index.html', { 'cats': cats})
