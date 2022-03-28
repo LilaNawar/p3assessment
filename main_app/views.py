ab28a946a1209734714a84c466c58740fc7a9b58
saadkhan29 <saad.iqbal@generalassemb.ly>
2022-03-28 15:54:04 -0400

    Cat Collector Models checkpoint 05

--- main_app/views.py
+++ main_app/views.py
@@ -27,3 +27,8 @@ def cats_index(request):
     cats = Cat.objects.all()
     # SELECT * from Cat
     return render(request, 'cats/index.html', { 'cats': cats})
+
+
+def cats_details(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    return render(request, 'cats/detail.html', {'cat': cat})
