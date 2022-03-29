643997c2392bc9a85204473fb29830ebbf1f5d1d
saadkhan29 <saad.iqbal@generalassemb.ly>
2022-03-29 16:08:13 -0400

    1toM checkpoint 04

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from .models import Cat
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
@@ -42,6 +42,14 @@ def cats_details(request, cat_id):
     cat = Cat.objects.get(id=cat_id)
     feeding_form = FeedingForm()
     return render(request, 'cats/detail.html', {'cat': cat, 'feeding_form': feeding_form})
+
+def add_feeding(request, cat_id):
+    form = FeedingForm(request.POST)
+    if form.is_valid():
+        new_feeding = form.save(commit=False)
+        new_feeding.cat_id = cat_id
+        new_feeding.save()
+    return redirect('detail', cat_id = cat_id)
 
 class CatUpdate(UpdateView):
     model = Cat
