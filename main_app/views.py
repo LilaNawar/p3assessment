77376ffddd4a36edeab3c65afe0c368344884130
saadkhan29 <saad.iqbal@generalassemb.ly>
2022-03-30 17:22:16 -0400

    MtoM checkpoint 04

--- main_app/views.py
+++ main_app/views.py
@@ -41,9 +41,7 @@ def cats_index(request):
 
 def cats_details(request, cat_id):
     cat = Cat.objects.get(id=cat_id)
-
     toys_cat_doesnt_have = Toy.objects.exclude(id__in = cat.toys.all().values_list('id'))
-
     feeding_form = FeedingForm()
     return render(request, 'cats/detail.html', {'cat': cat, 'feeding_form': feeding_form, 'toys': toys_cat_doesnt_have})
 
